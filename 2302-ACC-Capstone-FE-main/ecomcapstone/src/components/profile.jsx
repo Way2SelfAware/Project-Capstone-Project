9a9ec3888b3da8e4f4830becbf855e6e95854400
Way2SelfAware <neo@DESKTOP-DDPIL1P>
2023-08-26 16:22:25 -0700

    still working on profile display functionality

--- 2302-ACC-Capstone-FE-main/ecomcapstone/src/components/profile.jsx
+++ 2302-ACC-Capstone-FE-main/ecomcapstone/src/components/profile.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const Profile = () => {
+  // // state management
+  // const [user, setUser] = useState();
+
+  // //    fectch logic for user data will go here
+  // useEffect(() => {});
+
   return (
     <>
-      <div>Profile</div>
+      <div className="profile-page">
+        <h1>User Profile</h1>
+        {/* user profile pic */}
+        <div>
+          {user.profilePicture && (
+            <img
+              src={user.profilePicture}
+              alt={`${user.name}'s Account`}
+              className="profile-picture"
+            />
+          )}
+          {/* user pic upload */}
+          <input type="file" accept="image/*" />
+        </div>
+        {/* display user info */}
+        <p className="profile-user-data">
+          <strong>Name:</strong> {user.name}
+          <br />
+          <strong>Email:</strong> {user.email}
+          <br />
+          <strong>Address:</strong> {user.address}
+        </p>
+      </div>
     </>
   );
 };

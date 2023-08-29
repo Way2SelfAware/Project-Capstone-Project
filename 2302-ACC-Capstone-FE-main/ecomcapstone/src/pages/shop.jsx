af21c0c937c31b8e5267070e1044ad73d074ddc8
Way2SelfAware <neo@DESKTOP-DDPIL1P>
2023-08-28 19:27:48 -0700

    started working on getAllProducts and displaying

--- 2302-ACC-Capstone-FE-main/ecomcapstone/src/pages/shop.jsx
+++ 2302-ACC-Capstone-FE-main/ecomcapstone/src/pages/shop.jsx
@@ -1,28 +1,15 @@
+// react hooks
 import React from "react";
+// my components
+import AllProducts from "../components/allProducts";
 
 const Shop = () => {
   return (
     <>
       <div className="shop-page">
-        <h1>Shop</h1>
-        <p>
-          This will be used for shopping. Functionally must include:
-          <ol>
-            <li>UI/UX</li>
-            <li>View all available products.</li>
-            <li>
-              Sort products by certain characteristics (name, price, etc.).
-            </li>
-            <li>
-              Filter products by certain characteristics (category, type,
-              maximum/minimum price, etc.).
-            </li>
-            <li>
-              View the details for an individual product, including product
-              descriptions, photos, price, etc.
-            </li>
-          </ol>
-        </p>
+        <div className="products">
+          <AllProducts />
+        </div>
       </div>
     </>
   );

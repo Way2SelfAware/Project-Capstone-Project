d551af021a5e3206cd8a421f33fcbc74772f0064
Way2SelfAware <neo@DESKTOP-DDPIL1P>
2023-09-04 18:10:01 -0700

    started writing cart functionality: add, remove, update, calculate price

--- 2302-ACC-Capstone-FE-main/ecomcapstone/src/pages/cart.jsx
+++ 2302-ACC-Capstone-FE-main/ecomcapstone/src/pages/cart.jsx
@@ -1,24 +1,61 @@
+// react hooks
 import React from "react";
+import { useState } from "react";
+// my components
 
 const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  // add items to cart
+  const addToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
+
+  // remove items from cart
+  const removeFromCart = (productId) => {
+    const updatedCart = cartItems.filter((item) => item.id === productId);
+    setCartItems(updatedCart);
+  };
+
+  // update quantity of item in cart
+  const updateQuantity = (productId, newQuantity) => {
+    const updatedCart = cartItems.map((item) => {
+      if (item.id === productId) {
+        return { ...item, quantity: newQuantity };
+      }
+      return item;
+    });
+    setCartItems(updatedCart);
+  };
+
+  // calculate total price of cart
+  const calculateTotalPrice = () => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  };
+
   return (
-    <>
-      <div className="cart-page">
-        <h1>Cart</h1>
-        <p>
-          This will be used for the shopping cart. Functionally must include:
-          <ol>
-            <li>UI/UX</li>
-            <li>Add to cart with persistence</li>
-            <li>Change the quantity of a product in my cart. </li>
-            <li>Remove a product from my cart.</li>
-            <li>
-              "Checkout" the items in my cart, i.e., purchase the products.
-            </li>
-          </ol>
-        </p>
-      </div>
-    </>
+    <div className="cart-page">
+      <h1>Cart</h1>
+      <ul>
+        {cartItems.map((item) => (
+          <li key={item.id}>
+            {item.name} - ${item.price} - Quantity: {item.quantity}
+            <button onClick={() => removeFromCart(item.id)}>Remove</button>
+            <input
+              type="number"
+              value={item.quantity}
+              onChange={(e) =>
+                updateQuantity(item.id, parseInt(e.target.value))
+              }
+            />
+          </li>
+        ))}
+      </ul>
+      <p>Total Price: ${calculateTotalPrice()}</p>
+    </div>
   );
 };
 
